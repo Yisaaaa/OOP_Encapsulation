28c7a7f7d59f0b3ac0b3cf9b0c87e85b31f358e4
Jose Neil Silagan Jr. <neilsilagan@gmail.com>
2024-04-08 14:10:04 +0800

    add user role interface
    other commit wasn't saved

---
+++ RoleLogin.java
@@ -0,0 +1,59 @@
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.*;
+
+public class RoleLogin implements ActionListener {
+	
+	JFrame roleFrame;
+	JPanel loginPanel;
+	JButton admin;
+	JButton user;
+	
+	public static String pass() {
+		return "password";
+	}
+	
+	RoleLogin(){
+		roleFrame = new JFrame();
+		
+		loginPanel = new JPanel();
+		loginPanel.setLayout(new GridLayout(2,1,0,10));
+		loginPanel.setBounds(45, 100, 400, 250);
+		
+		admin = new JButton("Admin");
+		admin.addActionListener(this);
+		
+		user = new JButton("User");
+		user.addActionListener(this);
+		
+		roleFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		roleFrame.setLayout(null);
+		roleFrame.setSize(500, 500);
+		roleFrame.setResizable(false);
+		
+		loginPanel.add(admin);
+		loginPanel.add(user);
+		roleFrame.add(loginPanel);
+		roleFrame.setVisible(true);
+	}
+
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if (e.getSource() == admin) {
+			JPasswordField passwordField = new JPasswordField();
+			int option = JOptionPane.showConfirmDialog(roleFrame, passwordField, "Passcode:", JOptionPane.OK_CANCEL_OPTION, JOptionPane.PLAIN_MESSAGE);
+			if (option == JOptionPane.OK_OPTION) {
+				char[] enteredPass = passwordField.getPassword();
+				String enteredPassString = new String(enteredPass);
+				if (RoleLogin.pass().equals(enteredPassString)) {
+					System.out.println("Entered!");
+				} else {
+					JOptionPane.showMessageDialog(admin, "Wrong password!");
+				}
+			}
+		}
+	}
+}
+
