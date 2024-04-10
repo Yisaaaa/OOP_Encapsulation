07dcc2947f3baa518ff77fb81a2e8301e85867d9
Yisaaaa <ryansanisit19@gmail.com>
2024-04-10 14:58:47 +0800

    moved book covers into covers folder

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,6 @@ package src;
 public class Main {
 
     public static void main(String[] args) {
-        Book book = new Book("Subtle art of not giving a F*ck", "Mark Manson", "300", "What are the values that you should priority. Mark Manson also has a youtube channel that will help you be a better person each day of the month", "images/bookImages/subtleArtOfNotGivingAFuck.jpg");
-        
-        book.viewBook(book);
         new Home(true);
     }
 }
